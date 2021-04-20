198d6e7540944cc33e25e0c978d54f02abe4825d
Amresh Prasad Sinha <35039730+AmreshSinha@users.noreply.github.com>
2021-04-20 18:53:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import sklearn
 import joblib
-model = joblib.load('Movie Review Sentiment')
-st.title('Movie Sentiment')
+model = joblib.load('Coursera Review Sentiment')
+st.title('Coursera Review Sentiment')
 ip = st.text_input('Enter your review')
 op = model.predict([ip])
 if st.button('Predict'):
